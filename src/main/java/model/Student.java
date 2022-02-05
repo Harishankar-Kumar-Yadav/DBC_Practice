d932d4a67932e04b72c8a62f858e8d294a4ef278
Harishankar-Kumar-yadav <harishankar.yadav_cs19@gla.ac.in>
2022-02-05 13:30:27 +0530

    gatter setter method is applied

--- src/main/java/model/Student.java
+++ src/main/java/model/Student.java
@@ -7,8 +7,71 @@
 
 package model;
 
+import java.util.Objects;
+
 public class Student {
     private String firstName;
     private String lastName;
-    private String Roll
+    private String rollNumber;
+
+    public Student(String firstName, String lastName, String rollNumber) {
+        this.firstName = firstName;
+        this.lastName = lastName;
+        this.rollNumber = rollNumber;
+    }
+
+    public String study() {
+        return ("i am studying");
+    }
+
+    public String study(String subjectName) {
+        return ("i am studying" + subjectName);
+
+    }
+
+    public String getFirstName() {
+        return firstName;
+    }
+
+    public void setFirstName(String firstName) {
+        this.firstName = firstName;
+    }
+
+    public String getLastName() {
+        return lastName;
+    }
+
+    public void setLastName(String lastName) {
+        this.lastName = lastName;
+    }
+
+    public String getRollNumber() {
+        return rollNumber;
+    }
+
+    public void setRollNumber(String rollNumber) {
+        this.rollNumber = rollNumber;
+    }
+
+    @Override
+    public boolean equals(Object o) {
+        if (this == o) return true;
+        if (o == null || getClass() != o.getClass()) return false;
+        Student student = (Student) o;
+        return Objects.equals(firstName, student.firstName) && Objects.equals(lastName, student.lastName) && Objects.equals(rollNumber, student.rollNumber);
+    }
+
+    @Override
+    public int hashCode() {
+        return Objects.hash(firstName, lastName, rollNumber);
+    }
+
+    @Override
+    public String toString() {
+        return "Student{" +
+                "firstName='" + firstName + '\'' +
+                ", lastName='" + lastName + '\'' +
+                ", rollNumber='" + rollNumber + '\'' +
+                '}';
+    }
 }
