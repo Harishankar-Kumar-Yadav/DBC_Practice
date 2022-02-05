e414058c709112c5a0a087c8b3cac859beae037d
Harishankar-Kumar-yadav <harishankar.yadav_cs19@gla.ac.in>
2022-02-05 13:21:04 +0530

    initial comit

---
+++ src/main/java/card/Card.java
@@ -0,0 +1,11 @@
+/*
+ * Author Name: Harishankar Kumar Yadav
+ * IDE: IntelliJ IDEA Ultimate Edition
+ * Date: 03-02-2022
+ */
+
+
+package card;
+
+public class Card {
+}
---
+++ src/main/java/model/Student.java
@@ -0,0 +1,14 @@
+/*
+ * Author Name: Harishankar Kumar Yadav
+ * IDE: IntelliJ IDEA Ultimate Edition
+ * Date: 05-02-2022
+ */
+
+
+package model;
+
+public class Student {
+    private String firstName;
+    private String lastName;
+    private String Roll
+}
